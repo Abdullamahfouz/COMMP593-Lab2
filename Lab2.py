a3dfc09c7dc172f5dc2e2f53b7938dc719aa360f
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-01-25 18:51:00 -0500

    edited script

--- Lab2.py
+++ Lab2.py
@@ -1,61 +1,56 @@
 def main():
 
-    # TODO: Step 2 - Create a complex data structure
-    about_me = {
-        'name': 'Abdullah',
-        'Last name': 'Mahfouz',
-        'fav player':'Ronaldo',
-        'Most iconic' :'Ozil',
-        'Fav Footballers': [
-           'Ronaldo',
-            'Ozil',
-            'Mbappe',
-            'Ramos'
-         ],
-        'Ronaldo Vs Messi Goals' : [
+    # a complex data structure
+    fav_soccer_player = {
+        'name': 'Cristiano Ronaldo',
+        'nickname': 'CR7',
+        'player_best_team':'Real-Madrid',
+        'players_played_with': 
+            [
+             'Marclo',
+             'Pepe',
+             'benzema'
+        ],
+          
+        'Rivals':[
             { 
-             'player': 'Ronaldo',
-             'Goals' : 819,
-             'assits': 794
+             'Player': 'Messi',
+             'Goals' : 794,
+             'assits': 350
             },
             {
-                'player ': 'Messi',
-                'goals' : 819,
-                'assits': 500
+              'Player ': 'Cristiano Ronaldo',
+              'goals' : 819,
+              'Asists': 266
             }
         ]
     }
 
-    # TODO: Step 3 - Add another movie to the data structure
- 
-    print_Fav_players_name(about_me)
-    add_fav_teams(about_me, ('Real Madrdid', ' Man United', 'Liverpool'))
-    print_fav_teams(about_me)
-# TODO: Step 4 - Function that prints student name and ID	
-def print_Fav_players_name(Fav_players):
-    print(f"{Fav_players['name']} Thinks {Fav_players['Most iconic'].title()} will always be the best passer")
+    # Added New rival
+    fav_soccer_player['Rivals'].insert(1,{'Player': 'Zlatan Ibrahimović', 'Goals':  574, 'Assits': 157 })
+    add_other_fav_players(fav_soccer_player,('Ozil', 'Bale', 'ramos'))
+    print_player_name(fav_soccer_player)
+    # Function that prints the player nickname and name 
+def print_player_name(soccer_player):
+    print(f"{soccer_player['nickname']} Aka {soccer_player['name'].title()} has to be one of the best players of all-time for {soccer_player['']}  ")
+   
     
     
 # adds fav teams
-def add_fav_teams(fav_teams, teams):
-    fav_teams['teams'].extend(teams)
-    for i, p in enumerate(fav_teams['teams']):
-       fav_teams['teams'][i] = p.title()
-    
-    fav_teams['teams'].sort()
-    
-    pass 
+def add_other_fav_players(team,):
+   print( f'\n He played with players such as:')
+   for p in team['players_played_with']:
+       print(f'- {p}')
+       
     
 # prints fac teams
-def print_fav_teams(fav_teams):
-    print('\nThe team lis is:')
-    for t in fav_teams['teams']:
-        print(f'- {t}')
-  
+def print_Rivals(rival):
+   
+  return
 
-# TODO: Step 7 - Function that prints comma-separated list of movie genres
+# Step 7 - Function that prints comma-separated list of movie genres
 def print_movie_genres(Rivaly):
- return
+    return
    
 
 if __name__ == '__main__':
