d8c8cbbd7d468d27e6ca9785be091a09b25bf986
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-01-25 14:49:04 -0500

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,25 +1,62 @@
 def main():
 
     # TODO: Step 2 - Create a complex data structure
-    about_me = {}
+    about_me = {
+        'name': 'Abdullah',
+        'Last name': 'Mahfouz',
+        'fav player':'Ronaldo',
+        'Most iconic' :'Ozil',
+        'Fav Footballers': [
+           'Ronaldo',
+            'Ozil',
+            'Mbappe',
+            'Ramos'
+         ],
+        'Ronaldo Vs Messi Goals' : [
+            { 
+             'player': 'Ronaldo',
+             'Goals' : 819,
+             'assits': 794
+            },
+            {
+                'player ': 'Messi',
+                'goals' : 819,
+                'assits': 500
+            }
+        ]
+    }
 
     # TODO: Step 3 - Add another movie to the data structure
-    
+ 
+    print_Fav_players_name(about_me)
+    add_fav_teams(about_me, ('Real Madrdid', ' Man United', 'Liverpool'))
+    print_fav_teams(about_me)
 # TODO: Step 4 - Function that prints student name and ID	
-def print_student_name_and_id(about_me):
-    return
+def print_Fav_players_name(Fav_players):
+    print(f"{Fav_players['name']} Thinks {Fav_players['Most iconic'].title()} will always be the best passer")
     
-# TODO: Step 5 - Function that adds pizza toppings to data structure
-def add_pizza_toppings(about_me, toppings):
-    return
-
-# TODO: Step 6 - Function that prints bullet list of pizza toppings
-def print_pizza_toppings(about_me):
-    return
+    
+# adds fav teams
+def add_fav_teams(fav_teams, teams):
+    fav_teams['teams'].extend(teams)
+    for i, p in enumerate(fav_teams['teams']):
+       fav_teams['teams'][i] = p.title()
+    
+    fav_teams['teams'].sort()
+    
+    pass 
+    
+# prints fac teams
+def print_fav_teams(fav_teams):
+    print('\nThe team lis is:')
+    for t in fav_teams['teams']:
+        print(f'- {t}')
+  
 
 # TODO: Step 7 - Function that prints comma-separated list of movie genres
-def print_movie_genres(about_me):
-    return 
+def print_movie_genres(Rivaly):
+ return
+   
 
 if __name__ == '__main__':
     main()
